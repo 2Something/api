d2075ae730c1c465d72c6dbd78c1503826ac397d
Cookies <kalibstory.2010@outlook.com>
2025-01-31 22:05:49 -0500

    patch 1.0.1 BETA

--- pages/api/dfpwm.ts
+++ pages/api/dfpwm.ts
@@ -62,15 +62,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     console.log(chunks)
 
     const fp = ffmpeg(Readable.from(chunks))
-      .inputFormat("mp3")
+      //.inputFormat("mp3")
       .setFfmpegPath(require("@ffmpeg-installer/ffmpeg").path)
       .audioCodec("pcm_s8")
       .format("s8")
       .outputOptions("-ar", "48000")
+      .outputOptions("-f", "s8")
       .audioChannels(1)
       .pipe(outputStream)
 
     outputStream.on("data", chunk => pcmChunks.push(chunk))
+    outputStream.on("error", error => console.log("FFmpeg error: " + error))
     outputStream.on("end", () => {res.send(new dfpwm.Encoder().encode(Buffer.concat(pcmChunks)))})
   }
 }
