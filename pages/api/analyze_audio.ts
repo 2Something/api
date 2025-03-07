97c332bd9eac56bf803cba43e7780d7372a91b93
Cookies <kalibstory.2010@outlook.com>
2025-03-07 18:12:04 -0500

    Beta 1.1 - Fix analyze_audio API

--- pages/api/analyze_audio.ts
+++ pages/api/analyze_audio.ts
@@ -1,8 +1,4 @@
-const ffmpeg = require("fluent-ffmpeg")
-const path_probe = require("ffprobe-static").path
-const path_peg = require("ffmpeg-static").path
-
-console.log(path_probe)
+import {parseWebStream} from 'music-metadata'
 
 
 import { NextApiRequest, NextApiResponse } from "next";
@@ -11,28 +7,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (url && typeof(url) == "string") {
 
+        (async () => {
         try {
-            // Get the duration of the MP3 file from the URL
-            const duration = await getDurationFromUrl(url as string);
-            res.status(200).json({ duration });
+            // Assuming you have a ReadableStream of an audio file
+            const response = await fetch(url);
+            const webStream = response.body;
+
+            if (webStream) {
+
+                // Parse the metadata from the web stream
+                const metadata = await parseWebStream(webStream, 'audio/mpeg');
+
+                // Log the parsed metadata
+                res.send(metadata["format"]["duration"])
+            }
         } catch (error) {
-            console.error("Error fetching duration:", error);
-            res.status(500).json({ error: "Failed to fetch duration" });
+        console.error('Error parsing metadata:', error.message);
         }
+        })();
+        
     }
-}
-
-function getDurationFromUrl(url: string): Promise<number> {
-    return new Promise((resolve, reject) => {
-        ffmpeg.setFfmpegPath(path_peg)
-        ffmpeg.setFfprobePath(path_probe)
-
-        ffmpeg.ffprobe(url, (err, metadata) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(metadata.format.duration);
-                }
-            });
-    });
 }
